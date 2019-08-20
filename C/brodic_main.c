2a592898b5f78d996101ebff9d3ac9522369bf9e
milicazivkovic15 <38529691+milicazivkovic15@users.noreply.github.com>
2019-08-20 16:34:36 +0400

    Update brodic_main.c

--- C/brodic_main.c
+++ C/brodic_main.c
@@ -1,3 +1,12 @@
+/*
+Upotrebom semafora napraviti kontrolu rada ove tri vrste procesa imajući u vidu da:
+1. Brodic ima stalan kapacitet, npr. K.
+2. Brodic uvek kreće sa leve obale, putuje, iskrca putnike na desnoj, sačeka da se napune
+kapaciteti, pa se vraća na levu obalu, putuje, iskrca itd ...
+3. Na obali se nalazi kondukter, koji vodi evidenciju o putnicima koji čekaju na prevoz. Kada ih
+se nakupi dovoljno on ih, ako je brodić tu, pusti da se ukrcaju, mahne kapetanu da vozi, a
+broj ljudi koji čekaju na prevoz smanji za k.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
